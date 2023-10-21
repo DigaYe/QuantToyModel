c91604159e9fb04d89b30d384c2d973af9e56983
Diga Ye <digaye98@gmail.com>
2023-10-20 21:18:46 -0400

    Set up Environment

--- ReturnModel.py
+++ ReturnModel.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from util import st_charts as myChart
+#from util import st_charts as myChart
 import streamlit
 import bbgapi as bbgapi
 
@@ -9,3 +9,4 @@ bbg = bbgapi.BloombergAPI()
 x = bbg.bdh(["NFP T Index", "AWH TOTL Index", "AHE TOTL Index"], startdate="2006-03-01", per='M')
 x.columns = ["Jobs", "Hours", "Wages"]
 x['Total'] = x['Jobs'] * x['Hours'] * x['Wages']
+print(x)
